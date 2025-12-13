61ca533e2014c765e471ff94b967b7ce39edbf05
Marcelo Garcia <marcelo@mail.com>
2025-12-13 18:08:40 -0300

    Designing Angular Project

--- frontend/README.md
+++ frontend/README.md
@@ -1,3 +1,11 @@
 # Frontend Angular
 
 Starter Angular project deve ser criado pelo candidato.
+
+Steps:
+1. Config local angular version
+   npm install @angular/cli@19
+2. Check the version
+   ng version
+3. Create a project
+   ng new 
---
+++ frontend/new/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, 
+    MatToolbarModule, 
+    MatIconModule,
+    MatSidenavModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'app';
+  sidebarOpened = true;
+
+  toggleSidebar() {
+    this.sidebarOpened = !this.sidebarOpened;
+  }
+}
