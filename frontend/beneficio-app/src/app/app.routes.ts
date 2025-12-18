39512d6c4bb018048280fb4925a342c2954d22b7
Marcelo Garcia <marcelo@mail.com>
2025-12-18 08:09:18 -0300

    Removing old files

--- frontend/beneficio-app/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { BenefitComponent } from './benefit/benefit.component';
-import { AppComponent } from './app.component';
-
-export const routes: Routes = [
-    {path: 'benefits', component: BenefitComponent, title: 'Benefits Page'}
-];
--- frontend/beneficio-app/src/app/interfaces/benefit-account.ts
+++
@@ -1,7 +0,0 @@
-export interface BenefitAccount {
-    id: number;
-    nome: string;
-    descricao: string;
-    valor: number;
-    ativo: boolean;
-}
